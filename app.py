f23848dcdcd0bc25c52353c0ef4b4eb84bf87ad7
Georgy Mateev <gmateev@yahoo.com>
2025-08-14 12:21:18 +0300

    add files

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
 from fastapi import FastAPI
+import os
+import json
 
 app = FastAPI()
 
@@ -9,3 +11,9 @@ def home():
 @app.get("/path1")
 def home1():
     return {"message": "Hello from Render (/path1)!"}
+
+@app.get("/files")    
+def list_files():
+    files = [f for f in os.listdir('.') if os.path.isfile(f)]
+    #return files #json.dumps(files)
+    return {"files",json.dumps(files)}
