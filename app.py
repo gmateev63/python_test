2fc919271a40c23109ee520ebda9f94c2f371623
gmateev63 <94680934+gmateev63@users.noreply.github.com>
2025-09-24 09:38:47 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,11 +7,7 @@ app = FastAPI()
 
 @app.get("/")
 def home():
-    return {"message": "Hello from Render (/)!"}
-    
-@app.get("/path1")
-def home1():
-    return {"message": "Hello from Render (/path1)!"}
+    return {"message": "Hello from Render!"}
 
 @app.get("/files")    
 def list_files():
